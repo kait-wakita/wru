cb47a3b4fd607de3b269c85fc5821160467f2933
Toshihiro Wakita <wakita@cco.kanagawa-it.ac.jp>
2022-01-12 12:36:41 +0900

    fix misc/image.c to adapt static image

--- misc/image.c
+++ misc/image.c
@@ -337,7 +337,6 @@ void draw_detections_v3(image im, detection *dets, int num, float thresh, char *
     // text output
     qsort(selected_detections, selected_detections_num, sizeof(*selected_detections), compare_by_lefts);
     int i;
-    printf("Objects:\n");  // for darknet2ROS
     for (i = 0; i < selected_detections_num; ++i) {
         const int best_class = selected_detections[i].best_class;
         //printf("%s: %.0f%%", names[best_class],    selected_detections[i].det.prob[best_class] * 100);
@@ -366,6 +365,7 @@ void draw_detections_v3(image im, detection *dets, int num, float thresh, char *
             }
         }
     }
+    printf("Objects:\n");  // for darknet2ROS
 
     // image output
     qsort(selected_detections, selected_detections_num, sizeof(*selected_detections), compare_by_probs);
